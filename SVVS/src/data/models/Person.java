fda041572bdf2322b94d6c29cd4fe2b4d2766671
ffi0875 <florian.fink@students.fhv.at>
2012-10-25 23:48:33 +0200

    jojo halt so

---
+++ SVVS/src/data/models/Person.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data.models;
+
+import data.interfaces.IPerson;
+
+/**
+ *
+ * @author uubu
+ */
+public class Person implements IPerson {
+    
+    int personID;
+    String firstname;
+    String lastname;
+    
+}
