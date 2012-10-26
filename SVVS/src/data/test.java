cd0a503d7cc15080381e5b2763d5a9a8cc51b4a2
ffi0875 <florian.fink@students.fhv.at>
2012-10-26 05:15:12 +0200

    hibernate

--- SVVS/src/data/test.java
+++ SVVS/src/data/test.java
@@ -4,10 +4,32 @@
  */
 package data;
 
+import data.hibernate.HibernateUtil;
+import data.models.Country;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author Michael
  */
 public class test {
+    
+    public static void main(String args[]){
+        
+        Session s = HibernateUtil.getCurrentSession();
+        Transaction tx = s.beginTransaction();
+        
+        
+        Country country = new Country();
+        country.setCode("666");
+        country.setName("Dörkadörkistan");
+        
+        
+        s.save(country);
+
+        tx.commit();
+        
+    }
     
 }
